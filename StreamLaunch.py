4449d9fe2c9db8024e69473926a4c747a6065dce
Matt <mattc65@gmail.com>
2015-03-06 13:44:55 +0000

    Remove trailing whitespace from authcode, API was getting upset with me

--- StreamLaunch.py
+++ StreamLaunch.py
@@ -2,15 +2,23 @@ import urllib.request
 import json
 import urllib.parse
 from subprocess import call
+from http.client import BadStatusLine
+
+def getResponse():
+    try:
+        req = urllib.request.Request("https://api.twitch.tv/kraken/streams/followed", None, headers)
+        response = urllib.request.urlopen(req)
+        return response
+    except BadStatusLine:
+        print("Fetching failed, trying again...")
+        getResponse()
 
 authfile = open('authcode.txt')
 hashshit = authfile.readline() #idk how to advance line number
-auth = authfile.readline()
+auth = authfile.readline().strip()
 oauth = 'OAuth ' + auth
-headers = { 'Accept' : 'application/vnd.twitchtv.v2+json',
-            'Authorization' : oauth}
-req = urllib.request.Request("https://api.twitch.tv/kraken/streams/followed", None, headers)
-response = urllib.request.urlopen(req)
+headers = {'Authorization' : oauth}
+response = getResponse()
 jsonst = response.read().decode('utf-8')
 pyjson = json.JSONDecoder().decode(jsonst)
 
